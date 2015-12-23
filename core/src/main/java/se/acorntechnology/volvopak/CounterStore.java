f9eebc226e80fc021ff629233a789c9939123224
Chris Fogelklou <chris.fogelklou@gmail.com>
2015-12-23 18:24:31 +0100

    First commit of Lysvik's Spelet

---
+++ core/src/main/java/se/acorntechnology/volvopak/CounterStore.java
@@ -0,0 +1,13 @@
+package se.acorntechnology.volvopak;
+
+public class CounterStore {
+    private int count;
+
+    public void add(int num) {
+        count += num;
+    }
+
+    public int get() {
+        return count;
+    }
+}
